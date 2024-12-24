41b2a76dfad305567b2cbbc39e9ea019e91e16ab
Giorgi Kabanashvili <giorgi@kabanashvili.com>
2024-12-24 19:02:46 +0400

    fix time

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from time import gmtime, strftime
-import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+server_timezone = "Europe/Berlin"
+new_timezone = "Asia/Tbilisi"
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    t = time.strftime("%a, %d %b %Y %I:%M:%S %p %Z", time.gmtime())
-    return "Dear Guest, welcome on this simple web page showing the time: " + t
+    current_time = datetime.now(ZoneInfo(server_timezone))
+    t = current_time.astimezone(ZoneInfo(new_timezone))
+    return "Dear Guest, welcome on this simple web page showing the time: " + str(t)
 
 if __name__ == '__main__':
     app.run()
